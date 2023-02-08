163c064947926e455e4ff141a0011d3233dd2512
MrKearns <jkearns631@students.mchenry.edu>
2023-02-08 14:12:16 -0600

    first

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.text.DecimalFormat
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+// --------- VARS ----------
+var total = 0F
+val formatted = DecimalFormat("\u0024#,###.00")
+
+
+// ---------- RUNNING TOTAL FUNCTION ----------
+fun totalCalc(c: Float): Float {
+    total += c
+    return total
+}
+
+
+// ---------- MAIN FUNCTION ----------
+fun main(args: Array<String>?) {
+
+    //var total = 0
+
+    println("Enter cost of item(s). Or 0 when done:")
+    var cost = readln().toFloat()
+
+    when (cost) {
+        0F -> {
+            var tax = total * .025
+            var totalWithTax = total + tax
+            var suggestedTip = totalWithTax * .175
+            var formattedTotal = formatted.format(totalWithTax)
+            var formattedTip = formatted.format(suggestedTip)
+            println("Your total w/ tax is $formattedTotal")
+            println("Suggested Tip: $formattedTip ")
+
+            println("goodbye")
+        }
+
+        else -> {
+            totalCalc(cost)
+            main(null)
+        }
+    }
 }
